89a07c7f4e35eacf116fd137a98608926e1b6e9e
CMKana <ekanashkin2003@gmail.com>
2024-01-09 21:25:25 +1000

    Initial Commit

---
+++ Algorithms/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Algorithms
+//
+//  Created by Евгений Канашкин on 09.01.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
